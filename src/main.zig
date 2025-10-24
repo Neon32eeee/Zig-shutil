ae58fc9b6bd23b13914c32b1898b0db8b94aed10
NEon32eeee <pi9116759@gmail.com>
2025-10-24 18:00:21 +0300

    Fix main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,4 @@
-pub const cmd = @import("cmd.zig").cmd;
-pub const package = @import("package.zig").package;
-pub const user = @import("user.zig").user;
-pub const git = @import("git.zig").git;
+pub const cmd = @import("namespace/cmd.zig").cmd;
+pub const package = @import("namespace/package.zig").package;
+pub const user = @import("namespace/user.zig").user;
+pub const git = @import("namespace/git.zig").git;

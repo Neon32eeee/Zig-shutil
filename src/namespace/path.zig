982dff63675733f5fbdff193a02a9e505e324ce5
NEon32eeee <pi9116759@gmail.com>
2025-10-25 10:16:03 +0300

    New function 'cd' in 'path'

--- src/namespace/path.zig
+++ src/namespace/path.zig
@@ -9,19 +9,26 @@ const ShutilError = Core.ShutilError;
 // Namespace path utils |
 // ---------------------
 pub const path = struct {
-    // Get real path
+    // Returh real path
     pub fn realpath(settings: CmdSettings, source_path: []const u8) ![]const u8 {
         const command = [_][]const u8{ "realpath", source_path };
         return try CmdCallAndReturn(settings, &command);
     }
 
+    // Return file name without a path
     pub fn basename(settings: CmdSettings, source_path: []const u8) ![]const u8 {
         const command = [_][]const u8{ "basename", source_path };
         return try CmdCallAndReturn(settings, &command);
     }
 
+    // Returh path to file
     pub fn dirname(settings: CmdSettings, source_path: []const u8) ![]const u8 {
         const command = [_][]const u8{ "dirname", source_path };
         return try CmdCallAndReturn(settings, &command);
+    }
+
+    pub fn cd(settings: CmdSettings, target_path: []const u8) ![]const u8 {
+        const command = [_][]const u8{ "cd", target_path };
+        try CmdCall(settings, &command);
     }
 };

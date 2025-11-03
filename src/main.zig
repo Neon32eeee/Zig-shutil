1a8e1fcf4e9d1b44730a2c792c890057ec7d8587
NEon32eeee <pi9116759@gmail.com>
2025-11-03 10:34:50 +0300

    On namespace 'net' in main file

--- src/main.zig
+++ src/main.zig
@@ -3,3 +3,4 @@ pub const package = @import("namespace/package.zig").package;
 pub const user = @import("namespace/user.zig").user;
 pub const git = @import("namespace/git.zig").git;
 pub const path = @import("namespace/path.zig").path;
+pub const net = @import("namespace/net.zig").net;

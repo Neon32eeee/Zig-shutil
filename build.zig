3728f760fb9c8bad4bcacccfefc0c6ff6229360b
NEon32eeee <pi9116759@gmail.com>
2025-11-03 12:20:52 +0300

    Update build

--- build.zig
+++ build.zig
@@ -4,7 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Статическая библиотека
     const lib = b.addStaticLibrary(.{
         .name = "shutil",
         .root_source_file = b.path("src/main.zig"),
